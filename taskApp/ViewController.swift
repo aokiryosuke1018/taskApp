3f865b180c91465a0c71d3e44d96e6d21eaeccf3
ryosuke.aoki <ryo1018@live.jp>
2019-12-30 13:17:23 +0900

    Initial Commit

---
+++ taskApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taskApp
+//
+//  Created by 青木亮祐 on 2019/12/30.
+//  Copyright © 2019 ryosuke.aoki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
